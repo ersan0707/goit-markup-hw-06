204ace8237c85c6326974f64e1ad3fe10e61f3e5
Ersan Cibi <ersancibi@hotmail.com>
2025-03-17 23:43:19 +0300

    ödev6 çalışma3
    çalışma3

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const closeMenuButton = document.getElementById("closeMenuButton");
     const mobileMenu = document.getElementById("mobileMenu");
 
-    // Eğer butonlar veya menü bulunamazsa hata vermemesi için kontrol ekleyelim
     if (!burgerButton || !closeMenuButton || !mobileMenu) {
-        console.error("HATA: Menü öğeleri bulunamadı! ID'leri kontrol et.");
+        console.error("HATA: Menü öğeleri bulunamadı!");
         return;
     }
 
